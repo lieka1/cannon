8d3a391b752cf6b5afd18dda8899462d6a8a8e93
liekai <qscasdqwezxc@gmail.com>
2022-11-07 16:09:17 +0800

    rewrite loading

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,43 @@
-import {Game} from "phaser";
-import Main from './game'
-import LoadingScene from "./scenes/loading";
+import { Game } from "phaser";
+import Main from "./game";
 
 interface Window {
-  sizeChanged: () => void;
-  game: Phaser.Game;
+    sizeChanged: () => void;
+    game: Phaser.Game;
 }
 
 var STATIC: Window = {} as any;
 
 window.onload = () => {
-  STATIC.game = new Game({
-    type: Phaser.AUTO,
-    title: "loose cannon",
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: '#304858',
-    physics: {
-      default: 'arcade',
-      arcade: {
-        debug: true,
-      },
-    },
-    scene: [ LoadingScene, Main ]
-  });
+    let i = document.getElementById("loading");
+    if (i) i.remove();
+
+    STATIC.game = new Game({
+        type: Phaser.AUTO,
+        title: "loose cannon",
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: "#304858",
+        physics: {
+            default: "arcade",
+            arcade: {
+                debug: true,
+            },
+        },
+        scene: [Main],
+    });
 };
 
-
 const sizeChanged = () => {
-  if (STATIC.game.isBooted) {
-    setTimeout(() => {
-      STATIC.game.scale.resize(window.innerWidth, window.innerHeight);
-      STATIC.game.canvas.setAttribute(
-        'style',
-        `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
-      );
-    }, 100);
-  }
+    if (STATIC.game.isBooted) {
+        setTimeout(() => {
+            STATIC.game.scale.resize(window.innerWidth, window.innerHeight);
+            STATIC.game.canvas.setAttribute(
+                "style",
+                `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`
+            );
+        }, 100);
+    }
 };
 
 window.onresize = () => sizeChanged();
